7a3c23156106c2c379aa1c3e487a4150cef5abe1
damien75 <damiengoblot@gmail.com>
2014-02-04 10:47:09 +0100

    commit

--- app/src/main/java/sara/damien/app/WelcomeActivity.java
+++ app/src/main/java/sara/damien/app/WelcomeActivity.java
@@ -16,6 +16,9 @@ public class WelcomeActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mLocationClient = new LocationClient(this, this, this);
+
         setContentView(R.layout.activity_welcome);
 
         if (savedInstanceState == null) {
@@ -44,6 +47,19 @@ public class WelcomeActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
     }
 
     /**
