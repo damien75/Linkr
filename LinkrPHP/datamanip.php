10cf77bdddccacf676465ae6bd642c877dbd6f0d
sarafleur <sarafleursultan@gmail.com>
2014-02-11 09:25:24 +0100

    dd

--- LinkrPHP/datamanip.php
+++ LinkrPHP/datamanip.php
@@ -1,8 +1,5 @@
 <?php
-                                    /* Connexion à la base de données*/
-
-class Database {
-    public static function connect() {
+function connect(){
         $dsn = 'mysql:dbname=golinkrvv1;host=mysql51-113.perso';
         $user = 'golinkrvv1';
         $password = 's3y4ruZDxugK';
@@ -11,49 +8,21 @@ class Database {
             $dbh = new PDO($dsn, $user, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         } catch (PDOException $e) {
-            echo 'Connexion échouée : ' . $e->getMessage();
+            echo 'Connexion Ã©chouÃ©e : ' . $e->getMessage();
             exit(0);
         }
-        return $dbh;
-    }
-
+      return $dbh;
 }
 
-
-                                    /*Gestion des utilisateurs dans la table: utilisateurs*/
-
-class User {
-    // Company	Exp_Years	Sum_Grade	Number_Grade
-    public $ID;
-    public $Last_Name;
-    public $First_Name;
-    public $Loc_X;
-    public $Loc_Y;
-    public $Last_Subject;
-    public $Picture;
-    public $Company;
-    public $Exp_Years;
-    public $Sum_Grade;
-    public $Number_Grade;
-}
-
-    /*Imprimer les informations de tous les utilisateur*/
-function getUsers() {
-    $dbh = Database::connect();
-    $query = "SELECT * FROM User";
-    $sth = $dbh->prepare($query);
-    $sth->setFetchMode(PDO::FETCH_ASSOC);
-    $sth->execute(array());
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($result);
-}
-function getRequests($ID){
+   function getRequest($ID1){
+   $dbh=connect();
    $response = array();
-   $dbh = Database::connect();
-   $query = "SELECT Last_Name, First_Name, State, Date_Request, Date_Accept, Date_Meeting, Subject FROM user, (SELECT * FROM meeting WHERE ID1=?) as c WHERE ID=ID1";
+
+   $query = "SELECT Last_Name, First_Name, State, Date_Request, Date_Accept, Date_Meeting, Subject FROM user, (SELECT * FROM meeting WHERE ID1=?) as c WHERE ID=ID2";
    $sth = $dbh->prepare($query);
    $sth->setFetchMode(PDO::FETCH_ASSOC);
-   $sth->execute(array($ID));
+
+   $sth->execute(array($ID1));
    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
    
    
@@ -61,7 +30,7 @@ function getRequests($ID){
  
        $response["success"] = 1;
             $response["meeting"] = array();
-            while($row = next($result)){
+            while($row = current($result)){
             $meeting = array();
             $meeting["Last_Name"] = $row["Last_Name"];
             $meeting["First_Name"] = $row["First_Name"];
@@ -71,6 +40,7 @@ function getRequests($ID){
             $meeting["Date_Meeting"] = $row["Date_Meeting"];
             $meeting["Subject"] = $row["Subject"];
             array_push($response["meeting"], $meeting);
+            $row=next($result);
             }
             // echoing JSON response
             echo json_encode($response);
--- LinkrPHP/index.php
+++ LinkrPHP/index.php
@@ -1,25 +1,20 @@
 <?php
-header('Content-type: application/json');
-        $dsn = 'mysql:dbname=golinkrvv1;host=mysql51-113.perso';
-        $user = 'golinkrvv1';
-        $password = 's3y4ruZDxugK';
-        $dbh = null;
-        try {
-            $dbh = new PDO($dsn, $user, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-           // echo "doudss";
-        } catch (PDOException $e) {
-            echo 'Connexion échouée : ' . $e->getMessage();
-            exit(0);
-        }
-    $query = "SELECT `First_Name` FROM `user` WHERE ID1=1";
-    $sth = $dbh->prepare($query);    
-    $sth->setFetchMode(PDO::FETCH_ASSOC);
-    
-    $sth->execute(array());
-    echo "skj"; 
-    $result = $sth->fetch(PDO::FETCH_ASSOC);
-    echo $result;
-    
 
-?>
+header('Content-type: application/json');
+require('datamanip.php');
+if (isset($_POST["SELECT_FUNCTION"])){
+switch($_POST["SELECT_FUNCTION"])
+{
+    case 'getRequest';
+        if (isset($_POST["ID1"])) {
+         getRequest($_POST["ID1"]);}
+    break;
+    default;
+        echo 'Merci de faire un choix...';
+    break;
+}
+         
+
+}
+
+?>									
