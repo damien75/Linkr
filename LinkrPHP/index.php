98b6a001275fd8e42dfa287bcd3be5f0d56254b0
damien75 <damiengoblot@gmail.com>
2014-02-19 11:23:21 +0100

    test

--- LinkrPHP/index.php
+++ LinkrPHP/index.php
@@ -25,10 +25,18 @@ switch($_POST["SELECT_FUNCTION"])
             updateArchiveSubject($_POST["ID"],$_POST["subject"]);
         }
     break;
+    case 'getAllProfile';
+            getAllProfile();
+    break;
     case 'getLastSubject';
          if (isset($_POST["ID"])){
          getLastSubject ($_POST["ID"]);
          }
+    break;
+    case 'getProfileSupID';
+        if (isset($_POST["IDMIN"]) && isset($_POST["NBDOWN"]) && isset($_POST["XU"]) && isset($_POST["YU"]) && isset($_POST["E"])){
+            getProfileSupID($_POST["IDMIN"],$_POST["NBDOWN"],$_POST["XU"],$_POST["YU"],$_POST["E"]);
+        }
     break;
     default;
         echo 'Merci de faire un choix...';
