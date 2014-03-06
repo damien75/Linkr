d3bdfb9c6ca577c3a4645327ace1a6f87f73670e
sarafleur <sarafleursultan@gmail.com>
2014-03-06 10:31:53 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/sara/damien/app/utils/DateTimePicker.java
+++ app/src/main/java/sara/damien/app/utils/DateTimePicker.java
@@ -228,8 +228,8 @@ public class DateTimePicker extends RelativeLayout {
         }
     };
 
-    String[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-            "Sep", "Oct", "Nov", "Dec" };
+    String[] months = { "01", "02", "03", "04", "05", "06", "07", "08",
+            "09", "10", "11", "12" };
 
     View.OnClickListener month_plus_listener = new View.OnClickListener() {
 
