308c021eb0fce9f45b71f20d587d38b7a804160c
Clément Pit--Claudel <clement.pitclaudel@live.com>
2014-03-09 11:14:12 +0100

    Test end of lines

--- app/src/main/java/sara/damien/app/DB/DbHelper.java
+++ app/src/main/java/sara/damien/app/DB/DbHelper.java
@@ -22,6 +22,7 @@ import sara.damien.app.chat.Message;
 public class DbHelper extends SQLiteOpenHelper{
     // Logcat tag
     //Les loulous :)
+    //Test de fin de ligne
     private static final String LOG = "DatabaseHelper";
     // Database Version
     private static final int DATABASE_VERSION = 1;
