215e25d97f59c648dca69dceeb14fd4435213e25
Clément Pit--Claudel <clement.pitclaudel@live.com>
2014-03-12 20:09:57 +0100

    As discussed with SF, create a LinkrApplication subclass of Application, which initializes the Common singleton class in its onCreate method.
    Such initialization is required, because memory requirements can cause Android to terminate a background application and thereby wipe app state; subclassing Application seems to be the easiest way to restore Common upon reactivation of the app (http://developer.android.com/reference/android/app/Activity.html#ProcessLifecycle)

---
+++ app/src/main/java/sara/damien/app/LinkrApplication.java
@@ -0,0 +1,18 @@
+package sara.damien.app;
+
+import android.app.Application;
+
+/* NOTE: The doc (http://developer.android.com/reference/android/app/Application.html) suggests that
+ * subclassing Application is generally unneeded, and instead points to static singleton classes.
+ * The apporach implemented here is indeed to use singleton classes, but instead of passing an
+ * explicit Context every time the Common singleton instance is retrieved, we initialize it on app
+ * launch. http://stackoverflow.com/questions/987072/using-application-context-everywhere suggests
+ * this is fine.
+ */
+public class LinkrApplication extends Application {
+    @Override
+    public void onCreate () {
+        super.onCreate();
+        Common.Init(this);
+    }
+}
