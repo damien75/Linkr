2f3aa908bb77cea70900dc160104b9b52db1d396
damien75 <damiengoblot@gmail.com>
2014-03-15 10:25:46 +0100

    Resized the date and time picker and simplified its functions.

--- app/src/main/java/sara/damien/app/chat/MessageActivity.java
+++ app/src/main/java/sara/damien/app/chat/MessageActivity.java
@@ -5,7 +5,6 @@ import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,7 +13,6 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.Timer;
@@ -34,6 +32,9 @@ public class MessageActivity extends ListActivity {
     private String chateeID;
     public int hours;
     public int minute;
+    public int day;
+    public int month;
+    public int year;
     Dialog dialog;
 
     Meeting meeting;
@@ -51,8 +52,6 @@ public class MessageActivity extends ListActivity {
 
         Bundle bundle = getIntent().getExtras();
         meeting = bundle.getParcelable(BundleParameters.MEETING_KEY);
-        Calendar c = Calendar.getInstance();
-        Log.e("calendar",c.toString());
         this.setTitle(meeting.getOtherParticipant().getName());
         chateeID = meeting.getOtherParticipant().getID();
         timeStampReceived = Common.getPrefs().getLastReceivedMessageTimeStamp(chateeID);
@@ -66,14 +65,14 @@ public class MessageActivity extends ListActivity {
         scheduleNewMessageChecking();
     }
     public void sendMessage(View v) {
-            String message_text = text.getText().toString().trim(); //TODO: Disable send button unless getText() != ""
+        String message_text = text.getText().toString().trim(); //TODO: Disable send button unless getText() != ""
 
-            if (!message_text.isEmpty()){
-                text.setText("");
-                //TODO : ajouter les messages à la base locale même s'ils ne sont pas encore envoyés
-                Message message = new Message(message_text, chateeID);
-                addNewMessage(message);
-                message.send(adapter);
+        if (!message_text.isEmpty()){
+            text.setText("");
+            //TODO : ajouter les messages à la base locale même s'ils ne sont pas encore envoyés
+            Message message = new Message(message_text, chateeID);
+            addNewMessage(message);
+            message.send(adapter);
         }
     }
 
@@ -82,42 +81,24 @@ public class MessageActivity extends ListActivity {
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.datetimepicker);
         dialog.setCancelable(true);
-        dialog.setTitle("Please select a date");
+        dialog.setTitle("Select a date and time!");
         dialog.show();
 
-        TimePicker time_picker = (TimePicker) dialog.findViewById(R.id.timePicker);
-        time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
-        {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minutes)
-            {
-                // TODO Auto-generated method stub
-                // Toast.makeText(CustomDialog.this,
-                // "hourOfDay = "+hourOfDay+"minute = "+minute , 1000).show();
-                hours = hourOfDay;
-                minute = minutes;
-                //TextView msgDatePicker = (TextView) findViewById (R.id.messageDatePicker);
-                //msgDatePicker.setText(hourOfDay+" "+minutes);
-            }
-        });
 
+        final TimePicker time_picker = (TimePicker) dialog.findViewById(R.id.timePicker);
         final DatePicker date_picker = (DatePicker) dialog.findViewById(R.id.datePicker);
         Button btn = (Button) dialog.findViewById(R.id.buttonDateTimePicker);
         btn.setOnClickListener(new View.OnClickListener()
                                {
-
                                    public void onClick(View arg0)
                                    {
-                                       // TODO Auto-generated method stub
-
                                        TextView txt = (TextView) findViewById(R.id.messageDatePicker);
                                        txt.setText("You selected " + date_picker.getDayOfMonth() + "/" + (date_picker.getMonth() + 1) + "/"
                                                + date_picker.getYear());
-                                       txt.append("Time "+ hours + ":" +minute);
+                                       txt.append("Time "+ time_picker.getCurrentHour() + ":" + time_picker.getCurrentMinute());
 
                                        dialog.cancel();
                                    }
-
                                }
 
         );
